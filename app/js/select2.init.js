2541533f99e346555c7bb7baab7539ad7ec21cdd
Kamran Mmmdv <amorfrap@gmail.com>
2020-02-17 01:33:03 +0400

    Added select2 to drivers' search by lang filter

--- app/js/select2.init.js
+++ app/js/select2.init.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
     $('.select2MultipleDropDown').select2();
     $('.select2DropDownCountry').select2();
+    $('.select2MultipleDropDownLang').select2({
+        placeholder: {
+            id: '-1', // the value of the option
+            text: 'Select an option'
+        }
+    });
 });
